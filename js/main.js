ca401bc486a3e72cd11b7fbabf84f507463464c1
Ahmet Çağatay <ahmetsalihcagatay@gmail.com>
2024-04-09 12:25:55 +0300

    update today

--- js/main.js
+++ js/main.js
@@ -40,6 +40,7 @@ function initScene() {
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 300)
     camera.position.set(0, .5, 4).multiplyScalar(7);
+    camera.rotateX(-0.3);
 
     updateSceneSize();
 
@@ -53,7 +54,7 @@ function initScene() {
     topLight.shadow.camera.near = 5;
     topLight.shadow.camera.far = 400;
     scene.add(topLight);
-    
+
     createFloor();
     diceMesh = createDiceMesh();
     for (let i = 0; i < params.numberOfDice; i++) {
@@ -61,9 +62,10 @@ function initScene() {
         addDiceEvents(diceArray[i]);
     }
 
-    // throwDice();
+    throwDice();
 
     render();
+    
 }
 
 function initPhysics() {
@@ -127,7 +129,7 @@ function createDice() {
     });
     physicsWorld.addBody(body);
 
-    return {mesh, body};
+    return { mesh, body };
 }
 
 function createBoxGeometry() {
@@ -234,6 +236,7 @@ function addDiceEvents(dice) {
 
         dice.body.allowSleep = false;
 
+        
         const euler = new CANNON.Vec3();
         e.target.quaternion.toEuler(euler);
 
@@ -265,6 +268,7 @@ function addDiceEvents(dice) {
             // landed on edge => wait to fall on side and fire the event again
             dice.body.allowSleep = true;
         }
+        
     });
 }
 
@@ -295,16 +299,18 @@ function updateSceneSize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function throwDice(additionalScore = 0) {
+function throwDice(additionalScore = 0, color = "#fdfdfd") {
+    if (color) { changeDiceColor(color); }
     scoreResult.innerHTML = '';
 
     let totalScore = additionalScore; // Zarların ve eklenen skorun toplamını hesaplamak için bir değişken
+
 
     diceArray.forEach((d, dIdx) => {
         d.body.velocity.setZero();
         d.body.angularVelocity.setZero();
 
-        d.body.position = new CANNON.Vec3(6, dIdx * 1.5, 0);
+        d.body.position = new CANNON.Vec3(1, dIdx * 5, 7);
         d.mesh.position.copy(d.body.position);
 
         d.mesh.rotation.set(2 * Math.PI * Math.random(), 0, 2 * Math.PI * Math.random())
@@ -312,7 +318,7 @@ function throwDice(additionalScore = 0) {
 
         const force = 3 + 5 * Math.random();
         d.body.applyImpulse(
-            new CANNON.Vec3(-force, force, 0),
+            new CANNON.Vec3(-force, force, 10),
             new CANNON.Vec3(0, 0, .2)
         );
 
@@ -327,86 +333,207 @@ function throwDice(additionalScore = 0) {
     scoreResult.innerHTML = '' + totalScore.toString();
 }
 
+function changeDiceColor(color) {
+    // Eğer bir renk belirtilmemişse veya geçersiz ise, varsayılan beyaz rengi kullan
+    const defaultColor = new THREE.Color(0xeeeeee);
 
-// document.addEventListener('DOMContentLoaded', function () {
-    const ctx = document.getElementById('polar-area-chart').getContext('2d');
-    const categories = [
-      { name: 'INT', sub: { Craft: 0, Teach: 2, Learn: 1 }, color: 'rgb(68, 114, 196)' },
-      { name: 'WIS', sub: { Magic: 0, Spell: 0, Memory: 2 }, color: 'rgb(32, 56, 100)' },
-      { name: 'WIL', sub: { Mystic: 0, Focus: 1, Mental: 2 }, color: 'rgb(112, 48, 160)' },
-      { name: 'PER', sub: { Ranged: 0, Art: 2, Detect: 0 }, color: 'rgb(176, 99, 148)' },
-      { name: 'LCK', sub: { Critic: 2, Call: 0, Try: 0 }, color: 'rgb(59, 56, 56)' },
-      { name: 'CHA', sub: { Speak: 0, Drama: 0, Lead: 3 }, color: 'rgb(132, 60, 12)' },
-      { name: 'STR', sub: { Muscle: 0, Martial: 0, Heavy: 4 }, color: 'rgb(204, 0, 0)' },
-      { name: 'CON', sub: { Heal: 2, Live: 2, Pole: 0 }, color: 'rgb(255, 51, 0)' },
-      { name: 'AGI', sub: { Acrobat: 2, Karate: 0, Shield: 0 }, color: 'rgb(255, 192, 0)' },
-      { name: 'SPD', sub: { Hand: 2, Stealth: 1, Light: 0 }, color: 'rgb(112, 173, 71)' }
-    ];
-  
-    const subCategories = categories.flatMap(category =>
-      Object.keys(category.sub).map(sub => `${sub} - (${category.name})`)
-    );
-  
-    const subCategoryColors = categories.flatMap(category =>
-      Object.keys(category.sub).map(sub => category.color)
-    );
-  
-    const dataValues = categories.flatMap(category =>
-      Object.values(category.sub)
-    );
-  
-    const backgroundColors = subCategories.map((sub, i) =>
-      `${subCategoryColors[i].replace('rgb', 'rgba').replace(')', ', 0.8)')}`
-    );
-  
-    const borderColors = subCategoryColors;
-  
-    new Chart(ctx, {
-      type: 'polarArea',
-      data: {
+    // Eğer geçerli bir renk belirtilmişse, üç renk kanalı (R, G, B) değerlerini al
+    const newColor = color ? new THREE.Color(color).lerp(defaultColor, 0.5) : defaultColor;
+
+
+
+    // Her bir zarın renklerini değiştir
+    diceArray.forEach(dice => {
+        // İç ve dış zar olmak üzere iki farklı materyal vardır
+        dice.mesh.children.forEach(child => {
+            if (child.material instanceof THREE.MeshStandardMaterial) {
+                // Eğer materyal `THREE.MeshStandardMaterial` ise, rengi değiştir
+                child.material.color.copy(newColor);
+            }
+        });
+    });
+}
+
+
+
+let isReturning = false; // Zarların geri dönüş durumunu kontrol etmek için bir bayrak
+
+function returnDiceToView() {
+    const returnPosition = new CANNON.Vec3(0, 0, 0); // Zarların döneceği hedef konum
+    const returnQuaternion = new CANNON.Quaternion().setFromEuler(0, 0, 0); // Zarların döneceği hedef yön
+    const returnSpeed = 0.7; // Interpolasyon hızı, 0 ile 1 arasında bir değer
+
+    if (isReturning) return; // Eğer zaten geri dönüş işlemi başladıysa, işlevi tekrar başlatma
+    isReturning = true;
+
+    function animateReturn() {
+        let allReturned = true;
+        diceArray.forEach(dice => {
+            dice.body.position.lerp(returnPosition, returnSpeed, dice.body.position);
+            dice.body.quaternion.slerp(returnQuaternion, returnSpeed, dice.body.quaternion);
+            dice.mesh.position.copy(dice.body.position);
+            dice.mesh.quaternion.copy(dice.body.quaternion);
+
+            // Hedefe yeterince yakın olup olmadığını kontrol et
+            if (dice.body.position.distanceTo(returnPosition) > 0.01) {
+                allReturned = false;
+            }
+        });
+
+        if (!allReturned) {
+            requestAnimationFrame(animateReturn); // Tüm zarlar hedefe ulaşana kadar animasyonu sürdür
+        } else {
+            isReturning = false; // Geri dönüş işlemi tamamlandı
+        }
+    }
+    console.log("çek");
+    setTimeout(() => { // Zarları atıştan belirli bir süre sonra geri getir
+        animateReturn();
+    }, 3000); // 3 saniye sonra zarları geri getir
+}
+
+
+const ctx = document.getElementById('polar-area-chart').getContext('2d');
+
+const scoreResultElement = document.getElementById('score-result');
+const scoreTextElement = document.getElementById('score-text');
+
+const categories = [
+    { name: 'INTELLIGENCE', sub: { Craft: 0, Teach: 2, Learn: 1 }, color: 'rgb(68, 114, 196)' },
+    { name: 'WISDOM', sub: { Magic: 0, Spell: 0, Memory: 2 }, color: 'rgb(32, 56, 100)' },
+    { name: 'WILLPOWER', sub: { Mystic: 0, Focus: 1, Mental: 2 }, color: 'rgb(112, 48, 160)' },
+    { name: 'PERCEPTION', sub: { Ranged: 0, Art: 2, Detect: 0 }, color: 'rgb(176, 99, 148)' },
+    { name: 'LUCK', sub: { Critic: 2, Call: 0, Try: 0 }, color: 'rgb(59, 56, 56)' },
+    { name: 'CHARISMA', sub: { Speak: 0, Drama: 0, Lead: 3 }, color: 'rgb(132, 60, 12)' },
+    { name: 'STRENGTH', sub: { Muscle: 0, Martial: 0, Heavy: 4 }, color: 'rgb(204, 0, 0)' },
+    { name: 'CONSTITUTION', sub: { Heal: 2, Live: 2, Pole: 0 }, color: 'rgb(255, 51, 0)' },
+    { name: 'AGILITY', sub: { Acrobat: 2, Karate: 0, Shield: 0 }, color: 'rgb(255, 192, 0)' },
+    { name: 'SPEED', sub: { Hand: 2, Stealth: 1, Light: 0 }, color: 'rgb(112, 173, 71)' }
+];
+
+const subCategories = categories.flatMap(category =>
+    Object.keys(category.sub).map(sub => `${sub} - (${category.name})`)
+);
+
+const subCategoryColors = categories.flatMap(category =>
+    Object.keys(category.sub).map(sub => category.color)
+);
+
+const dataValues = categories.flatMap(category =>
+    Object.values(category.sub)
+);
+
+
+const backgroundColors = subCategories.map((sub, i) =>
+    `${subCategoryColors[i].replace('rgb', 'rgba').replace(')', ', 0.8)')}`
+);
+
+const borderColors = subCategoryColors;
+
+new Chart(ctx, {
+    type: 'polarArea',
+    data: {
         labels: subCategories,
         datasets: [{
-          data: dataValues,
-          backgroundColor: backgroundColors,
-          borderColor: borderColors,
-          borderWidth: 1
+            data: dataValues,
+            backgroundColor: backgroundColors,
+            borderColor: borderColors,
+            borderWidth: 1
         }]
-      },
-      options: {
+    },
+    options: {
         onClick: (event, activeElements) => {
             if (activeElements.length > 0) {
                 const activeIndex = activeElements[0].index;
                 console.log(activeIndex);
                 const activeValue = dataValues[activeIndex];
-                throwDice(activeValue);
+                const activeColor = subCategoryColors[activeIndex];
+                const activeText = subCategories[activeIndex];
+                throwDice(activeValue, activeColor);
+                scoreTextElement.innerHTML = activeText;
+                var scoreElements = document.getElementsByClassName('score');
+                for (var i = 0; i < scoreElements.length; i++) {
+                    scoreElements[i].style.borderColor = activeColor; // İstediğiniz renk kodunu kullanabilirsiniz
+                    scoreElements[i].style.color = activeColor; // İstediğiniz renk kodunu kullanabilirsiniz
+                }
+
+
             }
         },
         scales: {
-          r: {
-            max: dataValues, min: -1,
-            ticks: {
-              stepSize: 1,
-              display: false
-            },
-            grid: {
-              offset: true
+            r: {
+                max: dataValues, min: -1,
+                ticks: {
+                    stepSize: 1,
+                    display: false
+                },
+                grid: {
+                    offset: true
+                }
             }
-          }
         },
         plugins: {
-          datalabels: {
-            backgroundColor: context => context.dataset.backgroundColor,
-            borderRadius: 4,
-            color: 'black',
-            font: { weight: 'bold' },
-            formatter: (value) => value,
-          },
-          legend: {
-            display: false
-          }
+            datalabels: {
+                backgroundColor: context => context.dataset.backgroundColor,
+                borderRadius: 4,
+                color: 'black',
+                font: { weight: 'bold' },
+                // formatter: (value) => value,
+                formatter: (value, context) => {
+                    // Bu fonksiyon ile etiket metnini özelleştirebilirsiniz
+                    // Örneğin, 'heavy' etiketi için özel bir işlem yapmak istiyorsanız:
+                    if (context.chart.data.labels[context.dataIndex] === 'Heavy - (STRENGTH)') {
+                        return '★ ' + value; // 'Heavy' etiketli veri noktası için yıldız ekler
+                    }
+                    return value; // Diğer tüm veri noktaları için sadece değeri gösterir
+                }
+            },
+            legend: {
+                display: false
+            }
         }
-      }
+    }
+});
+
+
+function createHorizontalWalls() {
+    const wallThickness = 0.1; // Duvarların kalınlığı
+    const wallDepth = 40; // Duvarların derinliği, yani "yukarıdan aşağıya" uzanan boyut
+    const arenaSize = 8; // Arena boyutu, yani duvarların oluşturduğu kutunun genişliği ve uzunluğu
+
+    // Duvarların CANNON.js için pozisyonları ve boyutları (Yatay düzlemde)
+    const wallPositions = [
+        { pos: [window.innerWidth/200, 0, 0], size: [wallThickness, arenaSize, wallDepth] }, // Sağ duvar
+        { pos: [-window.innerWidth / 200, 0, 0], size: [wallThickness, arenaSize, wallDepth] }, // Sol duvar
+        // // { pos: [0, arenaSize / 2, 0], size: [arenaSize, wallThickness, wallDepth] }, // Ön duvar
+        { pos: [0, -arenaSize / 2, 0], size: [100, wallThickness, wallDepth] },  // Arka duvar
+        { pos: [0, 0, -2], size: [100, 100, 1] },  // Arka duvar
+        { pos: [0, 0, 17], size: [100, 100, 1] }  // Arka duvar
+    ];
+
+    wallPositions.forEach(wall => {
+        // Fizik duvarını yarat
+        const wallShape = new CANNON.Box(new CANNON.Vec3(...wall.size.map(x => x / 2)));
+        const wallBody = new CANNON.Body({
+            mass: 0,
+            position: new CANNON.Vec3(...wall.pos),
+            shape: wallShape,
+        });
+        physicsWorld.addBody(wallBody);
+
+        // Görsel duvarı yarat
+        const wallGeometry = new THREE.BoxGeometry(...wall.size);
+        // const wallMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 }); // Yeşil renk
+        const wallMaterial = new THREE.MeshStandardMaterial({
+            color: 0x00ff00, // Duvarların rengi, örneğin yeşil
+            transparent: true,
+            opacity: 0.5 // Duvarların yarı şeffaf olması için
+        });
+        
+        const wallMesh = new THREE.Mesh(wallGeometry, wallMaterial);
+        wallMesh.position.set(...wall.pos);
+        scene.add(wallMesh);
     });
-    
-//   });
-  
+}
+
+createHorizontalWalls();
